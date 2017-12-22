ff74869cd0426274f1e24e4e8faa7f50fd62bcb6
Darth.Veka <vedran.von.vitez@gmail.com>
2017-12-22 14:34:08 +0100

    Added sweetalert notification if account exists (needs refactoring)

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,5 @@
-import { firebase, googleProvider, facebookProvider, githubProvider } from '../firebase/firebase';
+import { firebase, googleProvider, facebookProvider, githubProvider, auth } from '../firebase/firebase';
+import swal from 'sweetalert2';
 
 export const login = (uid) => ({
     type: 'LOGIN',
@@ -7,19 +8,67 @@ export const login = (uid) => ({
 
 export const startGoogleLogin = () => {
     return () => {
-        return firebase.auth().signInWithPopup(googleProvider);
+        return firebase.auth().signInWithPopup(googleProvider).then((res) => {
+
+        }).catch((err) => {
+            firebase.auth().fetchProvidersForEmail(err.email).then((res) => {
+                const provider = res[0].split('.');
+                swal({
+                    position: 'center',
+                    type: 'error',
+                    title: 'Opps',
+                    text: `An account already exists with the same email address. Try login with ${provider[0]}.`,
+                    showConfirmButton: false,
+                    width: 400,
+                    padding: 30,
+                    timer: 5000
+                })
+            });
+        });
     }
 }
 
 export const startFacebookLogin = () => {
     return () => {
-        return firebase.auth().signInWithPopup(facebookProvider);
+        return firebase.auth().signInWithPopup(facebookProvider).then((res) => {
+
+        }).catch((err) => {
+            firebase.auth().fetchProvidersForEmail(err.email).then((res) => {
+                const provider = res[0].split('.');
+                swal({
+                    position: 'center',
+                    type: 'error',
+                    title: 'Opps',
+                    text: `An account already exists with the same email address. Try login with ${provider[0]}.`,
+                    showConfirmButton: false,
+                    width: 400,
+                    padding: 30,
+                    timer: 5000
+                })
+            });
+        });
     }
 }
 
 export const startGithubLogin = () => {
     return () => {
-        return firebase.auth().signInWithPopup(githubProvider);
+        return firebase.auth().signInWithPopup(githubProvider).then((res) => {
+
+        }).catch((err) => {
+            firebase.auth().fetchProvidersForEmail(err.email).then((res) => {
+                const provider = res[0].split('.');
+                swal({
+                    position: 'center',
+                    type: 'error',
+                    title: 'Opps',
+                    text: `An account already exists with the same email address. Try login with ${provider[0]}.`,
+                    showConfirmButton: false,
+                    width: 400,
+                    padding: 30,
+                    timer: 5000
+                })
+            });
+        });
     }
 }
 
